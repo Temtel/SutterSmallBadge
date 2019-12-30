71a34a676765929593cb34007e024415fc5eb5a0
Temtel <temtel@gmail.com>
2019-12-29 22:27:05 -0800

    Update Walk.ino

--- examples/Walk/Walk.ino
+++ examples/Walk/Walk.ino
@@ -1,16 +1,16 @@
 /*
-Grove LED Bar - Walk Example
+Sutter_Small_Badge - Walk Example
 This example will show you how to use setBits() function of this library.
 Set any combination of LEDs using 10 bits.
 This example walks through all 1024 (2^10) possible combinations.
 */
 
-#include <Grove_LED_Bar.h>
+#include <Sutter_Small_Badge.h>
 
 //BE SURE USE CORRESPONDING DEVICE
-//Grove_LED_Bar bar(6, 7, 0, LED_CIRCULAR_24);
+//Sutter_Small_Badge bar(6, 7, 0, LED_CIRCULAR_24);
 //FOR LED_BAR_10
-Grove_LED_Bar bar(7, 6, 0, LED_BAR_10); // Clock pin, Data pin, Orientation
+Sutter_Small_Badge(7, 6, 0, LED_BAR_10); // Clock pin, Data pin, Orientation
 
 void setup()
 {
